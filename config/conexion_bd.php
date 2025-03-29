cd12e8eaa11d81edb9685027752c5933008d735e
Jose Armando <armandorodriguezseg19@gmail.com>
2025-03-29 16:37:27 -0600

    Se cambio le utf8_bin del set charset por solo utf8

--- config/conexion_bd.php
+++ config/conexion_bd.php
@@ -8,5 +8,6 @@ if ($conexion->connect_error) {
 }
 
 // Configurar el juego de caracteres a UTF-8
-$conexion->set_charset('utf8_bin');
+//$conexion->set_charset('utf8_bin');
+$conexion->set_charset('utf8');
 ?>
